a982a3437c67186bc476bd8db3940e07f6516011
oussamachah2020 <otaksam213@gmail.com>
2024-02-07 16:37:55 +0100

    REF(library): changing the library mapbox -> leaflet

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoib3Vzc2FtYTIwMjMiLCJhIjoiY2xzYmtoZDkxMDJzYzJtbWhrN3VuMDhxNiJ9.20vQPo1K1sLhHZgzpIejkw";
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/examples/clg45vm7400c501pfubolb0xz",
-  center: [-7.6498092, 33.5427328],
-  zoom: 12,
-});
+// mapboxgl.accessToken =
+//   "pk.eyJ1Ijoib3Vzc2FtYTIwMjMiLCJhIjoiY2xzYmtoZDkxMDJzYzJtbWhrN3VuMDhxNiJ9.20vQPo1K1sLhHZgzpIejkw";
+// const map = new mapboxgl.Map({
+//   container: "map",
+//   style: "mapbox://styles/examples/clg45vm7400c501pfubolb0xz",
+//   center: [-7.6498092, 33.5427328],
+//   zoom: 12,
+// });
 
 const geojson = {
   type: "FeatureCollection",
@@ -14,63 +14,63 @@ const geojson = {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.6403525, 33.5430359],
+        coordinates: [33.53429699201245 - 7.655013079892126],
       },
     },
     {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.655013079892126, 33.53429699201245],
+        coordinates: [33.554841993241006 - 7.64820922442506],
       },
     },
     {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.64820922442506, 33.554841993241006],
+        coordinates: [33.54459186787761 - 7.626240916200977],
       },
     },
     {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.628911195046597, 33.52106216198838],
+        coordinates: [33.54555389298911 - 7.630915008696994],
       },
     },
     {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.626240916200977, 33.54459186787761],
+        coordinates: [33.564656579904394 - 7.641992527238289],
       },
     },
     {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.630915008696994, 33.54555389298911],
+        coordinates: [33.52925355000557 - 7.635706695475866],
       },
     },
     {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.641992527238289, 33.564656579904394],
+        coordinates: [33.52371472866993 - 7.644165575289867],
       },
     },
     {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.635706695475866, 33.52925355000557],
+        coordinates: [33.515138207344506 - 7.654714200491618],
       },
     },
     {
       type: "Feature",
       geometry: {
         type: "Point",
-        coordinates: [-7.644165575289867, 33.52371472866993],
+        coordinates: [33.529752078836246 - 7.653074968300015],
       },
     },
     {
@@ -87,38 +87,46 @@ const geojson = {
         coordinates: [-7.653074968300015, 33.529752078836246],
       },
     },
-
   ],
 };
 
+// // add markers to map
+// geojson.features.forEach(function (marker) {
+//   // create a HTML element for each feature
 
+//   // make a marker for each feature and add to the map
+//   new mapboxgl.Marker().setLngLat(marker.geometry.coordinates).addTo(map);
+// });
 
-// add markers to map
+// map.on("load", function () {
+//     const coordinates = geojson.features.map(feature => feature.geometry.coordinates);
+
+//     const lats = coordinates.map(coord => coord[1]);
+//     const lngs = coordinates.map(coord => coord[0]);
+
+//     const maxLat = Math.max(...lats);
+//     const minLat = Math.min(...lats);
+//     const maxLng = Math.max(...lngs);
+//     const minLng = Math.min(...lngs);
+
+//     map.fitBounds([[minLng, minLat], [maxLng, maxLat]], {
+//       padding: 50
+//     });
+
+//     geojson.features.forEach(function (marker) {
+//         new mapboxgl.Marker().setLngLat(marker.geometry.coordinates).addTo(map);
+//     });
+//   });
+
+var map = L.map("map").setView([33.5430359, -7.6403525], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
 geojson.features.forEach(function (marker) {
-  // create a HTML element for each feature
-
-  // make a marker for each feature and add to the map
-  new mapboxgl.Marker().setLngLat(marker.geometry.coordinates).addTo(map);
+  var marker = L.marker(marker.geometry.coordinates).addTo(map);
+  marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 });
-
-
-map.on("load", function () {
-    const coordinates = geojson.features.map(feature => feature.geometry.coordinates);
-  
-    const lats = coordinates.map(coord => coord[1]);
-    const lngs = coordinates.map(coord => coord[0]);
-  
-    const maxLat = Math.max(...lats);
-    const minLat = Math.min(...lats);
-    const maxLng = Math.max(...lngs);
-    const minLng = Math.min(...lngs);
-  
-    map.fitBounds([[minLng, minLat], [maxLng, maxLat]], {
-      padding: 50
-    });
-  
-    geojson.features.forEach(function (marker) {
-        new mapboxgl.Marker().setLngLat(marker.geometry.coordinates).addTo(map);
-    });
-  });
-  
